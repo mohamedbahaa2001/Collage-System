5ed6624031a0fffaa5f15554195e873ad94e9efd
Mohamed Bahaa <mohamedbahaa1970@gmail.com>
2021-12-10 16:12:36 +0200

    first commit

---
+++ home/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from home import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('home', views.home, name='home'),
+    # path('about', views.about, name='about'),
+    path('material', views.material, name='material'),
+    path('contact', views.contact, name='contact'),
+    path('userprofile', views.userprofile, name='userprofile'),
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+]
---
+++ home/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from home import models
+# Create your views here.
+
+
+def home(request):
+    # can pass variables to html page
+    # context = {
+    #     'name':'mohamed',
+    #     'learning': 'web'
+    # }
+    # return HttpResponse("this is my homepage (/)")
+    return render(request, 'home.html')
+
+
+
+def material(request):
+    return render(request, 'material.html')
+
+def contact(request):
+    if request.method == 'POST':
+        # do stuff
+        print("Got Post Request")
+        name = request.POST['name']
+        email = request.POST['email']
+        message = request.POST['message']
+        ins = models.Contact(name=name, email=email, message=message)
+        ins.save()
+        print("data has been saved")
+    return render(request, 'contact.html')
+
+
+def userprofile(reguest):
+    return render(reguest, 'UserProfile.html')
+
+
+def login(reguest):
+    return render(reguest, 'login.html')
+
+def register(reguest):
+    return render(reguest, 'register.html')
